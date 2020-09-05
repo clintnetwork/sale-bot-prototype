c0c8d3660bd9b952d6ca038528e5e2d51c69f9e4
Clint.Network <me@clint.network>
2020-09-05 20:42:37 +0200

    Init

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace sale_bot_prototype
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
